e0e80f3012844f3ed8efafe2bbfbe14d6ab56c0e
Junyoung <tom9744@gmail.com>
2021-03-04 16:20:55 +0900

    Typescript Base Component Class 선언 및 상속을 통해 중복된 코드 제거하기

--- [2021.02.20] Typescript 고급 기능/Exercise/src/app.ts
+++ [2021.02.20] Typescript 고급 기능/Exercise/src/app.ts
@@ -125,25 +125,78 @@ function AutoBind(_target: any, _methodName: string, descriptor: PropertyDescrip
     return modifiedDescriptor
 }
 
-// Project List Class
-class ProjectList {
-    templateElem: HTMLTemplateElement;
-    actualElem: HTMLElement;
-    destinationElem: HTMLDivElement;
+/**
+ * Project Base Class 
+ * 
+ * 추상 클래스로 선언하여, 직접 인스턴스를 생성할 수 없도록 한다.
+ * 
+ * 또한, targetElement와 contentElement의 경우 HTMLElement 타입을 기본으로 하지만,
+ * 경우에 따라 보다 세부적인 타입을 사용할 수 있도록 Generic 타입으로 설정한다.
+ */
+abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+    templateElement: HTMLTemplateElement;
+    targetElement: T;
+    contentElement: U;
 
+    /**
+     * Optional Parameter는 생략될 수 있으므로 반드시 마지막에 위치해야 한다.
+     * @param templateElementId 
+     * @param targetElementId
+     * @param insertAtBeginning
+     * @param newElementId (Optional)
+     */
+    constructor(
+        templateElementId: string, 
+        targetElementId: string, 
+        insertAtBeginning: boolean, 
+        newElementId?: string
+    ) {
+        this.templateElement = document.getElementById(templateElementId)! as HTMLTemplateElement; 
+        this.targetElement = document.getElementById(targetElementId)! as T;
+
+        // .content를 이용해 <template> 태그의 내부에 접근할 수 있으며, 
+        // importNode() 메서드를 이용해 document에 외부 문서 내용을 삽입한다.
+        // importNode() 메서드의 두번째 매개변수는 Deep Copy 여부이다. 
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.contentElement = importedNode.firstElementChild as U;
+
+        if (newElementId)
+        {
+            this.contentElement.id = newElementId;
+        }
+
+        this.attachElement(insertAtBeginning);
+    }
+
+    private attachElement(insertAtBeginning: boolean) {
+        if (insertAtBeginning) {
+            this.targetElement.insertAdjacentElement("afterbegin", this.contentElement);
+        }
+        else {
+            this.targetElement.insertAdjacentElement("beforeend", this.contentElement);
+        }
+    }
+
+    abstract configureComponent(): void;
+
+    abstract renderComponent(): void;
+}
+
+// Project List Class
+class ProjectList extends Component<HTMLDivElement, HTMLElement>{
     assignedProjects: Project[] = [];  // 인스턴스에 배정된 Proejct의 목록을 저장하는 배열.
 
     /**
      * type 변수의 타입은 문자열 'active' 또는 'finished'만 가질 수 있는 Literal Union 타입이다. 
      */
     constructor(private type: "active" | "finished") {
-        this.templateElem = document.getElementById("project-list")! as HTMLTemplateElement; 
-        this.destinationElem = document.getElementById("app")! as HTMLDivElement;
+        super("project-list", "app", false, `${type}-projects`);
 
-        const importedNode = document.importNode(this.templateElem.content, true);
-        this.actualElem = importedNode.firstElementChild as HTMLElement;
-        this.actualElem.id = `${this.type}-projects`;
-
+        this.configureComponent();
+        this.renderComponent();
+    }
+    
+    configureComponent() {
         // 전역 변수로 등록된 ProjectState에 Listener를 등록해 구독한다.
         globalProjectState.addListener((projects: Project[]) => {
             // Javascript Array 내장 함수 filter()와 enum 타입을 사용해 필터링 기능을 구현한다.
@@ -152,15 +205,17 @@ class ProjectList {
                     return project.status === ProjectStatus.Active;
                 }
                 return project.status === ProjectStatus.Finished;
-            });
-
+            }); 
             this.assignedProjects = relevantProjects;
             this.renderProjects();
         });
-    
-        this.attach();
-        this.renderContent();
-    } 
+    }
+
+    renderComponent() {
+        const listId = `${this.type}-projects-list`;
+        this.contentElement.querySelector("ul")!.id = listId;
+        this.contentElement.querySelector("h2")!.textContent = `${this.type.toUpperCase()} PROJECT`;
+    }
 
     // 구독한 ProjectState에 변화가 발생해 'projects'가 반환되면, 화면에 렌더링한다.
     private renderProjects() {
@@ -174,50 +229,34 @@ class ProjectList {
             listElem.appendChild(listItem); 
         }
     }
-
-    private renderContent() {
-        const listId = `${this.type}-projects-list`;
-        this.actualElem.querySelector("ul")!.id = listId;
-        this.actualElem.querySelector("h2")!.textContent = `${this.type.toUpperCase()} PROJECT`;
-    }
-
-    private attach() {
-        this.destinationElem.insertAdjacentElement("beforeend", this.actualElem);
-    }
 }
-
+ 
 // Project Input Class
-class ProjectInput {
-    templateElem: HTMLTemplateElement;
-    actualElem: HTMLElement;
-    destinationElem: HTMLDivElement;
-
+class ProjectInput extends Component<HTMLDivElement, HTMLFormElement>{
     titleInputElem: HTMLInputElement;
     descriptionInputElem: HTMLInputElement;
     peopleInputElem: HTMLInputElement;
 
     constructor() {
-        this.templateElem = document.getElementById("project-input")! as HTMLTemplateElement; 
-        this.destinationElem = document.getElementById("app")! as HTMLDivElement; 
+        super("project-input", "app", true, "user-input");
 
-        /* 
-        ** .content를 이용해 <template> 태그의 내부에 접근할 수 있으며, 
-        ** importNode() 메서드를 이용해 document에 외부 문서 내용을 삽입한다.
-        ** importNode() 메서드의 두번째 매개변수는 Deep Copy 여부이다.
-        */ 
-        const importedNode = document.importNode(this.templateElem.content, true);
-        this.actualElem = importedNode.firstElementChild as HTMLElement;
-        this.actualElem.id = "user-input";
-        /*
-        ** <form> 태그 하위에 있는 <input> 태그들을 ID를 이용해 접근한다.
-        */ 
-        this.titleInputElem = this.actualElem.querySelector("#title") as HTMLInputElement;
-        this.descriptionInputElem = this.actualElem.querySelector("#description") as HTMLInputElement;
-        this.peopleInputElem = this.actualElem.querySelector("#people") as HTMLInputElement;
+        // <form> 태그 하위에 있는 <input> 태그들을 ID를 이용해 접근한다. 
+        this.titleInputElem = this.contentElement.querySelector("#title") as HTMLInputElement;
+        this.descriptionInputElem = this.contentElement.querySelector("#description") as HTMLInputElement;
+        this.peopleInputElem = this.contentElement.querySelector("#people") as HTMLInputElement;
 
-        this.configure();
-        this.attachElem();
+        this.configureComponent();
     }
+
+    configureComponent() {
+        // bind(this)를 통해 submitHandler()의 this를 클래스(인스턴스)로 변경해야 한다.
+        // 그렇지 않으면, submitHandler() 내부의 this는 'actualElem'가 된다. 
+        //
+        // 화살표 함수가 아닌 일반 함수는, 해당 함수를 호출한 대상으로 this의 context가 변경되기 때문이다.        
+        this.contentElement.addEventListener('submit', this.submitHandler);
+    }
+
+    renderComponent() {}
 
     /*
     ** [문자열, 문자열, 숫자] 형태의 Tuple을 반환 타입으로 갖는 메서드지만,
@@ -277,21 +316,6 @@ class ProjectInput {
 
             this.clearInputFields();
         }
-    }
-
-    private configure() {
-        /**
-         * bind(this)를 통해 submitHandler()의 this를 클래스(인스턴스)로 변경해야 한다.
-         * 그렇지 않으면, submitHandler() 내부의 this는 'actualElem'가 된다. 
-         * 
-         * 화살표 함수가 아닌 일반 함수는, 해당 함수를 호출한 대상으로 this의 context가 변경되기 때문이다.
-         **/       
-        this.actualElem.addEventListener('submit', this.submitHandler);
-        // this.actualElem.addEventListener('submit', this.submitHandler.bind(this));
-    }
-
-    private attachElem() {
-        this.destinationElem.insertAdjacentElement('afterbegin', this.actualElem);
     }
 }
 
