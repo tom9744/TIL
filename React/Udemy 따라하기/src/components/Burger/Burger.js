6d3d8635606024c10af55e01cd41e07fc241efa1
Junyoung <tom9744@gmail.com>
2021-03-25 23:34:01 +0900

    React - Udemy 프로젝트 따라하기, Props를 이용한 Component 동적 렌더링

--- React/Udemy 따라하기/src/components/Burger/Burger.js
+++ React/Udemy 따라하기/src/components/Burger/Burger.js
@@ -4,11 +4,17 @@ import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
+    const convertedIngredients = Object.keys(props.ingredients)
+        .map((key) => {
+            return [...Array(props.ingredients[key])].map((_, idx) => {
+                return <BurgerIngredient key={key + idx} type={key}/>
+            });
+        }); 
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {convertedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
