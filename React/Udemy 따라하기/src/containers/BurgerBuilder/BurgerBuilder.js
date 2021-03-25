26353f2c93e94ded561f7c29c69a903a4b4bc180
Junyoung <tom9744@gmail.com>
2021-03-25 22:24:20 +0900

    React - Udemy 프로젝트 따라하기, Component와 Container 생성

---
+++ React/Udemy 따라하기/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+import Aux from "../../hoc/Auxiliary"
+
+class BurgerBuilder extends Component {
+    render () {
+        return (
+            <Aux>
+                <div>Burger</div>
+
+                <div>Builder Controller</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
