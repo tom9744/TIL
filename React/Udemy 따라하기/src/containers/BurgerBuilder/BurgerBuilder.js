da6bdacf7c771dcfb0e3b5ccc3db56f7f7f3cf3c
Junyoung <tom9744@gmail.com>
2021-03-25 22:46:26 +0900

    React - Udemy 프로젝트 따라하기, PropTypes 추가 / Burger 렌더링

--- React/Udemy 따라하기/src/containers/BurgerBuilder/BurgerBuilder.js
+++ React/Udemy 따라하기/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 
 import Aux from "../../hoc/Auxiliary"
+import Burger from "../../components/Burger/Burger"
 
 class BurgerBuilder extends Component {
     render () {
         return (
             <Aux>
-                <div>Burger</div>
+                <Burger></Burger>
 
                 <div>Builder Controller</div>
             </Aux>
