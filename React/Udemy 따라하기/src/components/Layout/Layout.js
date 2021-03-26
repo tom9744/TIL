c33f28c23aa1d3ef3e155bdf509778e3e41e93a9
Junyoung <tom9744@gmail.com>
2021-03-26 13:04:36 +0900

    React - Udemy 프로젝트 따라하기, Toolbar Component / Logo Component / NavigationItems Component 추가

--- React/Udemy 따라하기/src/components/Layout/Layout.js
+++ React/Udemy 따라하기/src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from "react"
 
-import Aux from "../../hoc/Auxiliary"
+import Aux from "../../hoc/Auxiliary";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
-import classes from "./Layout.css"
+import classes from "./Layout.css";
 
 const layout = (props) => (
     <Aux>
-        <div>Toolbar, Navbar, Backdrop...</div>
+        <Toolbar></Toolbar>
         <main className={classes.Content}>
             {props.children}
         </main>
